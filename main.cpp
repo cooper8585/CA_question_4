f8140b22d6099920b01541b9d73bef07a06ab585
cooper8585 <pawel.bednarz85@gmail.com>
2024-07-09 17:33:57 +0000

    q4 answer

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "mbed.h"
+
+int main() {
+    
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    // Add values of a and b
+    int d = a + b;
+
+    // Print value of d
+    printf("Value of d: %d\n", d);
+
+    // Subtract c from d
+    int e = d - c;
+
+    // Print value of e
+    printf("Value of e: %d\n", e);
+}
+
